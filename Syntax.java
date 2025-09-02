c70c8396ab80c8a7b68bf84a57f0b7bcc5e7e742
THUMMALA CHAKRAVARTHI <thummalachakravarthi2002@gmail.com>
2025-09-02 11:03:56 +0530

    Create Syntax.java

---
+++ Syntax.java
@@ -0,0 +1,23 @@
+// Using string literal
+String str1 = "Hello";
+
+// Using constructor
+String str2 = new String("World");
+
+// Empty StringBuilder
+StringBuilder sb1 = new StringBuilder();
+
+// StringBuilder with initial content
+StringBuilder sb2 = new StringBuilder("Hello World");
+
+// Append example
+sb2.append("!!!");  // adds text at the end
+// Empty StringBuffer
+StringBuffer sbf1 = new StringBuffer();
+
+// StringBuffer with initial content
+StringBuffer sbf2 = new StringBuffer("Hello World");
+
+// Append example
+sbf2.append("!!!");  // adds text at the end
+
